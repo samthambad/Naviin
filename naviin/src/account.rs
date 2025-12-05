6647382fe61f73fe559c1e384ecfe6920ce81541
samthambad <98635284+samthambad@users.noreply.github.com>
2025-12-05 14:29:57 +0800

    Add rudimentary username input

---
+++ naviin/src/account.rs
@@ -0,0 +1,10 @@
+struct Account {
+    name: String,
+    balance: u32,
+    skill_level: Skill,
+}
+
+enum Skill {
+    Beginner,
+    Expert,
+}
--- naviin/src/main.rs
+++ naviin/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, Write};
+
+mod account;
+
 fn main() {
-    println!("Hello, world!");
+    let mut username = String::new();
+
+    println!("Enter login details");
+    print!("Username:");
+    io::stdout().flush().unwrap(); // to make sure it prints
+    io::stdin()
+        .read_line(&mut username)
+        .expect("Invalid username entered");
+}
+
+fn validate(&String username) -> bool {
+    // check against storage
 }
