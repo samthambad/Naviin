2d8865b367b0ae7b184aa5963d0ea844cfb360f5
samthambad <98635284+samthambad@users.noreply.github.com>
2025-12-06 11:56:30 +0800

    Deposit and display functions for AppState

--- naviin/src/AppState.rs
+++ naviin/src/AppState.rs
@@ -2,18 +2,25 @@ use std::{collections::HashMap, hash::Hash};
 
 use crate::Finance::{Holding, Symbol, Trade};
 
-struct AppState {
+pub struct AppState {
     cash_balance: f64,
     holdings: HashMap<Symbol, Holding>,
     trades: Vec<Trade>,
 }
 
 impl AppState {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             cash_balance: 0.0,
             holdings: HashMap::new(),
             trades: Vec::new(),
         }
+    }
+
+    pub fn deposit(&mut self, amount: f64) {
+        self.cash_balance += amount;
+    }
+    pub fn display(&self) {
+        println!("Cash balance: {}", self.cash_balance)
     }
 }
