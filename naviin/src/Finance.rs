cfb96108dedd292c0adbee68cbb888d3d8281e03
samthambad <98635284+samthambad@users.noreply.github.com>
2025-12-06 11:20:17 +0800

    Add some important structs in Finance

--- naviin/src/Finance.rs
+++ naviin/src/Finance.rs
@@ -1,1 +1,24 @@
-fn fund(amount: f64) {}
+pub fn fund(amount: f64) {
+    println!("Fund amount: {amount}");
+}
+
+pub type Symbol = String;
+
+pub struct Holding {
+    name: String,
+    quantity: f64,
+    avg_cost: f64,
+}
+
+pub struct Trade {
+    symbol: Symbol,
+    quantity: f64,
+    price_per: f64,
+    side: Side,
+    timestamp: i64, // epoch seconds
+}
+
+pub enum Side {
+    Buy,
+    Sell,
+}
