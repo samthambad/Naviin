dc1d8e09f6ee3e48e8eee66291f0e246a05b0396
samthambad <98635284+samthambad@users.noreply.github.com>
2025-12-06 11:19:56 +0800

    Initializer for AppState

--- naviin/src/AppState.rs
+++ naviin/src/AppState.rs
@@ -1,5 +1,19 @@
+use std::{collections::HashMap, hash::Hash};
+
+use crate::Finance::{Holding, Symbol, Trade};
+
 struct AppState {
     cash_balance: f64,
     holdings: HashMap<Symbol, Holding>,
-    trades: Vec<Trades>,
+    trades: Vec<Trade>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            cash_balance: 0.0,
+            holdings: HashMap::new(),
+            trades: Vec::new(),
+        }
+    }
 }
