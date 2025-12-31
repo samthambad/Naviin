d12a4b2a9f3cbdf55e37086f49fe02ff15a11352
samthambad <thambad@gmail.com>
2025-12-31 13:13:27 +0800

    Change command parsing to use match

--- naviin/src/main.rs
+++ naviin/src/main.rs
@@ -19,64 +19,69 @@ async fn main() {
             .read_line(&mut command)
             .expect("Invalid command entered");
         command = command.trim().to_string();
-        if command == "fund" {
-            print!("Amount: ");
-            io::stdout().flush().unwrap();
-            let mut fund_amount = String::new();
-            io::stdin()
-                .read_line(&mut fund_amount)
-                .expect("Invalid amount entered");
-            let fund_amount: f64 = fund_amount.trim().parse().unwrap();
-            Finance::fund(&state, fund_amount).await;
-            Storage::save_state(&state);
-        }
-        if command == "display" {
-            state.lock().unwrap().display().await;
-        }
-        if command == "withdraw" {
-            print!("Amount: ");
-            io::stdout().flush().unwrap();
-            let mut withdraw_amount = String::new();
-            io::stdin()
-                .read_line(&mut withdraw_amount)
-                .expect("Invalid amount entered");
-            let withdraw_amount: f64 = withdraw_amount.trim().parse().unwrap();
-            Finance::withdraw(&state, withdraw_amount).await;
-            Storage::save_state(&state);
-        }
-        if command == "price"
-            && let Some(ticker) = UserInput::ask_ticker()
-        {
-            FinanceProvider::previous_price_close(&ticker, true).await;
-        }
-        if command == "buy" {
-            Finance::buy(&state).await;
-            Storage::save_state(&state);
-        }
-        if command == "buylimit" {
-            let new_limit_order = Finance::create_limit_order().await;
-            if let Some(order) = new_limit_order {
-                {
-                    let mut state_guard = state.lock().unwrap();
-                    state_guard.add_open_order(order);
-                }
+        match command.as_str() {
+            "fund" => {
+                print!("Amount: ");
+                io::stdout().flush().unwrap();
+                let mut fund_amount = String::new();
+                io::stdin()
+                    .read_line(&mut fund_amount)
+                    .expect("Invalid amount entered");
+                let fund_amount: f64 = fund_amount.trim().parse().unwrap();
+                Finance::fund(&state, fund_amount).await;
                 Storage::save_state(&state);
-                println!("Open order added");
-            }
-        }
-        if command == "sell" {
-            Finance::sell(&state).await;
-            Storage::save_state(&state);
-        }
-        if command == "reset" {
-            Storage::default_state();
-        }
-        if command == "help" {
-            UserInput::display_help();
-        }
-        if command == "exit" {
-            Storage::save_state(&state);
-            break;
+            },
+            "display" => state.lock().unwrap().display().await,
+            "withdraw" => {
+                print!("Amount: ");
+                io::stdout().flush().unwrap();
+                let mut withdraw_amount = String::new();
+                io::stdin()
+                    .read_line(&mut withdraw_amount)
+                    .expect("Invalid amount entered");
+                let withdraw_amount: f64 = withdraw_amount.trim().parse().unwrap();
+                Finance::withdraw(&state, withdraw_amount).await;
+                Storage::save_state(&state);
+            },
+            "price" => {
+                if let Some(ticker) = UserInput::ask_ticker() {
+                    FinanceProvider::previous_price_close(&ticker, true).await;
+                }
+            },
+            "buy" => {
+                Finance::buy(&state).await;
+                Storage::save_state(&state);
+            },
+            "buylimit" => {
+                let new_limit_order = Finance::create_limit_order().await;
+                if let Some(order) = new_limit_order {
+                    {
+                        let mut state_guard = state.lock().unwrap();
+                        state_guard.add_open_order(order);
+                    }
+                    Storage::save_state(&state);
+                    println!("Open order added");
+                }
+            },
+            "sell" => {
+                Finance::sell(&state).await;
+                Storage::save_state(&state);
+            },
+            "reset" => Storage::default_state(), // TODO: not working
+            "help" => UserInput::display_help(),
+            "exit" => {
+                Storage::save_state(&state);
+                break;
+            },
+            "stopbg" => {
+                running.store(false, std::sync::atomic::Ordering::Relaxed);
+                println!("All background orders will be paused till relaunch of app or `startbg` command");
+            },
+            "startbg" => {
+                running.store(true, std::sync::atomic::Ordering::Relaxed);
+                println!("Background orders resumed execution");
+            },
+            _ => println!("Wrong command entered"),
         }
     }
 }
